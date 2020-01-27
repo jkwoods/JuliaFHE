9318ac5f50e8e37790c69ccb2f87d2d51e4d3ca6
Jess Woods <45497968+jkwoods@users.noreply.github.com>
2020-01-27 17:18:47 -0500

    bug fixes
    still need to fix mod - turns out every single programming language does neg mods differently so that's awesome

--- main.jl
+++ main.jl
@@ -14,11 +14,15 @@ l       = 10
 
 Encrypt, Decrypt, Recrypt, Add, Mult = Scheme.generate(lam,rho,eta,gam,Theta,alpha,tau,l)
 
+println("Encrypt")
+
 one = [1,1,1,1,1,1,1,1,1,1]
 zero = [0,0,0,0,0,0,0,0,0,0]
 
 one_c = Encrypt(one)
 zero_c = Encrypt(zero)
+
+println("Decrypt")
 
 one_d = Decrypt(one_c)
 zero_d = Decrypt(zero_c)
