695d2e02780845c72bc364d9992c7e44193b81eb
Jess Woods <45497968+jkwoods@users.noreply.github.com>
2020-07-29 11:29:37 -0400

    Update main.jl
    from raptor

--- main.jl
+++ main.jl
@@ -3,54 +3,36 @@ println("Running Tests")
 println("Making Scheme")
 
 
-lam     = 12
-rho     = 26
-eta     = 1988
-gam     = 147456
-Theta   = 150
-alpha   = 936
-tau     = 412
-l       = 10
+function test()
+    lam = 42
+    rho = 26
+    eta = 988
+    gam = 290000
+    Theta = 150
+    alpha = 936
+    tau = 188
+    l = 10
 
-Encrypt, Decrypt, Recrypt, Add, Mult, KeyCorrect = Scheme.generate(lam,rho,eta,gam,Theta,alpha,tau,l)
-println("Key Made")
+    Encrypt, Decrypt, Recrypt, Add, Mult, KeyCorrect = Scheme.generate(lam,rho,eta,gam,Theta,alpha,tau,l)
+    println("Key Time")
+    @time Encrypt, Decrypt, Recrypt, Add, Mult, KeyCorrect = Scheme.generate(lam,rho,eta,gam,Theta,alpha,tau,l)
 
-println(KeyCorrect())
+    a = rand(0:1,l)
+    b = rand(0:1,l)
 
-one  = [1,1,1,1,1,1,1,1,1,1]
-zero = [0,0,0,0,0,0,1,0,0,0]
+    ca = Encrypt(a)
+    println("Encrypt Time")
+    @time cb = Encrypt(b)
 
-println("Encrypt")
-c1 = Encrypt(one)
-c0 = Encrypt(zero)
-#cb = Encrypt(br)
+    Decrypt(ca)
+    println("Decrypt Time")
+    @time Decrypt(cb)
 
-println(Decrypt(c1))
-println(Decrypt(c0))
+    ra = Recrypt(ca)
+    println("Recrypt Time")
+    @time rb = Recrypt(cb)
 
-#Decrypt_sq(c0)
 
-println(Decrypt(Recrypt(c1)))
-println(Decrypt(Recrypt(c0)))
+end
 
-a = Mult(c1,c0)
-println(Decrypt(a))
-
-a = Recrypt(a)
-println(Decrypt(a))
-
-a = Mult(c1,a)
-println(Decrypt(a))
-
-b = Mult(a,c1)
-println(Decrypt(b))
-
-#=
-a = Recrypt(a)
-println(Decrypt(a))
-
-g = Mult(a,c1)
-println(Decrypt(g))
-
-g = Recrypt(g)
-println(Decrypt(g))=#
+test()
